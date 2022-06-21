1288a590fdb94e0168824afa7546ee66649f8392
LucasLopes <lopes.sp.lucas@gmail.com>
2022-06-21 15:56:48 -0300

    resolve requisito 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from './components/Header';
+import Missions from './components/Missions';
 import Solarsystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <>
         <Head />
         <Solarsystem />
+        <Missions />
       </>
     );
   }
