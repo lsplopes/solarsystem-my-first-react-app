ce0baa168b7aa5283b2a171796c686cdf7376924
LucasLopes <lopes.sp.lucas@gmail.com>
2022-06-21 15:01:35 -0300

    resolve requisitos 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Head from './components/Header';
+import Solarsystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Head />
+      <>
+        <Head />
+        <Solarsystem />
+      </>
     );
   }
 }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Solarsystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+
+export default Solarsystem;
